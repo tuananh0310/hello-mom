f362266fafd6f50bb89efa9e0fbcdc2636e93bb3
Le Tuan Anh <anhlxt.sdintern01@gmail.com>
2023-08-13 23:06:48 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                console.log(entry);
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                } else {
+                    entry.target.classList.remove("show");
+                }
+            });
+        }
+    );
+
+    const hiddenElements = document.querySelectorAll(".hidden");
+
+    console.log(hiddenElements);
+
+    hiddenElements.forEach((element) => {
+        observer.observe(element);
+    });
+});
